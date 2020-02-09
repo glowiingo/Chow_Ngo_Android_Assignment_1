3ce81fa4a67fb55c1698f0674a6cc9c34bdf330a
glowiingo <gloria.nhuto.ngo@gmail.com>
2020-02-08 16:34:08 -0800

    created details + suggestions activitiy

---
+++ app/src/main/java/com/example/android_asn_1/Suggestions.java
@@ -0,0 +1,14 @@
+package com.example.android_asn_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Suggestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suggestions);
+    }
+}
