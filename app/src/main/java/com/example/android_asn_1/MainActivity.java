ebcaccda75b287fba5749bafc340f84acad22e62
glowiingo <gloria.nhuto.ngo@gmail.com>
2020-02-08 16:58:15 -0800

    updated manifest and build grade to add volley dependency

--- app/src/main/java/com/example/android_asn_1/MainActivity.java
+++ app/src/main/java/com/example/android_asn_1/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.android_asn_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Access the RequestQueue through your singleton class.
+
     }
 
     void makeAPICall () {
@@ -20,4 +24,6 @@ public class MainActivity extends AppCompatActivity {
         return "https://newsapi.org/v2/everything?q=" + query
                 + "&sortBy=publishedAt&apiKey=789a4eb72dd04d369213df40d906db11";
     }
+
+
 }
