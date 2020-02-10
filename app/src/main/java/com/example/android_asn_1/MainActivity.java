642a2570759d1904e77b409e8c28498b4f50de64
glowiingo <gloria.nhuto.ngo@gmail.com>
2020-02-10 14:15:44 -0800

    added volley toast request and response

--- app/src/main/java/com/example/android_asn_1/MainActivity.java
+++ app/src/main/java/com/example/android_asn_1/MainActivity.java
@@ -3,27 +3,77 @@ package com.example.android_asn_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import org.json.JSONObject;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+public class MainActivity extends AppCompatActivity {   private static final String TAG = MainActivity.class.getName();
+    private Button btnRequest;
+
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Access the RequestQueue through your singleton class.
 
+        btnRequest = (Button) findViewById(R.id.button);
+
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                queryInput(v);
+                sendAndRequestResponse();
+            }
+        });
     }
 
-    void makeAPICall () {
+    private void sendAndRequestResponse() {
 
+        //RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        //String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(getApplicationContext(),"Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.i(TAG,"Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
     }
 
-    String generateURL (String query) {
+    public void queryInput(View view) {
+        EditText inputText = (EditText) findViewById(R.id.inputText);
+        String inputTextString = inputText.getText().toString();
+        url = generateURL(inputTextString);
+    }
+
+    public String generateURL (String query) {
         return "https://newsapi.org/v2/everything?q=" + query
                 + "&sortBy=publishedAt&apiKey=789a4eb72dd04d369213df40d906db11";
     }
+
 
 
 }
